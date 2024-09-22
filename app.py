0e4b90c26ce158488ca20520506e9c72ba7094fb
J Duane <69173891+DuaneIndustries@users.noreply.github.com>
2024-09-22 11:18:25 -0400

    Update app.py
    v2

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import io
 import requests
 from datetime import datetime, timedelta
 
-url='https://raw.githubusercontent.com/DuaneIndustries/StackStreetInstall/refs/heads/main/StackStreetProject_v1.csv'
+url='https://raw.githubusercontent.com/DuaneIndustries/StackStreetInstall/refs/heads/main/StackStreetProject_v2.csv'
 s=requests.get(url).content
 df=pd.read_csv(io.StringIO(s.decode('utf-8')))
 
